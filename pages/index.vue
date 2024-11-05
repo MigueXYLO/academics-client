de053ffabee64941f41897ddb82692817ca26a1e
Miguel Silva <2221462@my.ipleiria.pt>
2024-11-05 15:39:15 +0000

    TEsting

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
-  <div v-if="error">Error: {{ error.message }}</div>
-  <div v-else>
-    <nuxt-link to="/create">Create a New Student</nuxt-link>
-    <nuxt-link to="/index_courses">View Courses</nuxt-link>
-    <h2>Students</h2>
-    <table>
-      <thead>
-      <tr><th>Username</th><th>Name</th><th>E-mail</th><th>Course</th></tr>
-      </thead>
-      <tbody>
-      <tr v-for="student in students">
-        <td>{{ student.username }}</td>
-        <td>{{ student.name }}</td>
-        <td>{{ student.email }}</td>
-        <td>{{ student.courseName }}</td>
-      </tr>
-      </tbody>
-    </table>
+  <div>
+    <h1>Welcome to Academics Management</h1>
+    <nuxt-link to="/students">Students</nuxt-link>
   </div>
-  <button @click.prevent="refresh">Refresh Data</button>
 </template>
-<script setup>
-const config = useRuntimeConfig()
-const api = config.public.API_URL
-const { data: students, error, refresh } = await useFetch(`${api}/student`)
-</script>
---
+++ pages/students/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="error">Error: {{ error.message }}</div>
+  <div v-else>
+    <nuxt-link to="/create">Create a New Student</nuxt-link>
+    <nuxt-link to="/index_courses">View Courses</nuxt-link>
+    <h2>Students</h2>
+    <table>
+      <thead>
+      <tr><th>Username</th><th>Name</th><th>E-mail</th><th>Course</th><th>Actions</th></tr>
+      </thead>
+      <tbody>
+      <tr v-for="student in students">
+        <td>{{ student.username }}</td>
+        <td>{{ student.name }}</td>
+        <td>{{ student.email }}</td>
+        <td>{{ student.courseName }}</td>
+        <nuxt-link :to="`/students/${student.username}`">Details</nuxt-link>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  <button @click.prevent="refresh">Refresh Data</button>
+</template>
+<script setup>
+const config = useRuntimeConfig()
+const api = config.public.API_URL
+const { data: students, error, refresh } = await useFetch(`${api}/student`)
+</script>
